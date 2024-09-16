3f6e67579b8145c952b46c42e160efe4cb9c56b7
Maxence Walter <maxencewalter@MacBook-Air-de-Maxence.local>
2024-09-16 16:38:26 +0200

    chore: [NTF-1] - Initialise SwiftUi App

---
+++ NutriFit/NutriFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriFitApp.swift
+//  NutriFit
+//
+//  Created by Maxence Walter on 16/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct NutriFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
